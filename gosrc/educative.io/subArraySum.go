476afe979e4df07c40be94bd9619f49bfd16b0da
Karan <karan.shh1@gmail.com>
2021-03-07 02:44:44 +0530

    add some test cases those are cool

--- gosrc/educative.io/subArraySum.go
+++ gosrc/educative.io/subArraySum.go
@@ -14,3 +14,21 @@ func findAverageContiguousSubArrays(arr []int, k int) []float64 {
 	}
 	return resultArray
 }
+
+func findMaxSumContiguousSubArrays(arr []int, k int) int {
+	var max int
+	max = -222222
+	windowStart := 0
+	windowSum := 0
+	for windowEnd := 0; windowEnd < len(arr); windowEnd++ {
+		windowSum += (arr[windowEnd])
+		if windowEnd >= k-1 {
+			if max <= windowSum {
+				max = windowSum
+			}
+			windowSum -= (arr[windowStart])
+			windowStart++
+		}
+	}
+	return max
+}
--- gosrc/educative.io/subArraySum_test.go
+++ gosrc/educative.io/subArraySum_test.go
@@ -32,3 +32,40 @@ func Test_findAverageContiguousSubArrays(t *testing.T) {
 		})
 	}
 }
+
+func Test_findMaxSumContiguousSubArrays(t *testing.T) {
+	type args struct {
+		arr []int
+		k   int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "karan1",
+			args: args{
+				arr: []int{2, 1, 5, 1, 3, 2},
+				k:   3,
+			},
+			want: 9,
+		},
+		{
+			name: "karan12",
+			args: args{
+				arr: []int{2, 3, 4, 1, 5},
+				k:   2,
+			},
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findMaxSumContiguousSubArrays(tt.args.arr, tt.args.k); got != tt.want {
+				t.Errorf("findMaxSumContiguousSubArrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
