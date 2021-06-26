ca17a7fe9574372737acfdf2c9b4d36957202c24
Karan <karan.shh1@gmail.com>
2021-06-26 17:20:19 +0530

    lc-35

---
+++ gosrc/lc/lc-35.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+/*
+
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+Example 4:
+
+Input: nums = [1,3,5,6], target = 0
+Output: 0
+
+Example 5:
+
+Input: nums = [1], target = 0
+Output: 0
+
+*/
+func main() {
+	fmt.Println(searchInsertPosition([]int{1, 3, 5, 6}, 0))
+	fmt.Println(searchInsertPosition([]int{1, 3, 5, 6}, 7))
+
+	fmt.Println(searchInsertPosition([]int{1}, 0))
+
+}
+
+func searchInsertPosition(nums []int, target int) int {
+	low := 0
+	high := len(nums) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return low
+}
