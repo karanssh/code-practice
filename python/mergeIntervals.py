5870b99d35a48dec91a0d1aafbd83e98ed7ff774
karanssh <karan.shh1@gmail.com>
2023-01-03 01:53:55 +0530

    add more test code for lamda

--- python/mergeIntervals.py
+++ python/mergeIntervals.py
@@ -52,3 +52,28 @@ mergeExclusive(
     intervals= [Interval(3,7),Interval(1,2), Interval(11,13)],
     insertPoint=Interval(2,6)
 )
+
+
+def mergeNonClass(intervals: List[List]):
+    if len(intervals)< 2:
+        return 
+    
+    intervals.sort(key=lambda x: x[0])
+    mergedIntervals = [] 
+    start = intervals[0].start 
+    end =  intervals[0].end
+    for i in range(1, len(intervals)):
+        nextInterval = intervals[i]
+        if end >= nextInterval.start:
+            end = max(end, nextInterval.end)
+        else:
+            mergedIntervals.append(Interval(start, end))
+            start = nextInterval.start
+            end = nextInterval.end
+    mergedIntervals.append(Interval(start, end)) 
+    return mergedIntervals
+
+
+mergeNonClass(
+    intervals= [[3,7],[1,2], [5,6]]
+)
