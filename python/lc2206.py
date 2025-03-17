688f6e1bf0bdb436cd89da3dd2b7329fd9d8a5cc
Karan <karan.shh1@gmail.com>
2025-03-17 17:55:38 +0530

    lol

---
+++ python/lc2206.py
@@ -0,0 +1,10 @@
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        if len(nums)%2!=0:
+            return False
+        c = Counter(nums)
+        for n in c:
+            if c[n]%2!=0:
+                return False
+        
+        return True
