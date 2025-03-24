f8e3b0c90e967106076bb5f9b9e084e8543f8601
karanssh <karan.shh1@gmail.com>
2025-03-24 14:18:44 +0530

    one more way

--- python/lc22.py
+++ python/lc22.py
@@ -21,3 +21,21 @@ class Solution:
         
         backtrack(0,0)
         return res
+    
+    def generateParenthesis2(self, n: int) -> List[str]:
+
+        res = []
+
+        def backtrack(open_n, closed_n, path):
+            if open_n == closed_n == n:
+                res.append(path)
+                return
+            if open_n < n:
+                backtrack(open_n + 1, closed_n, path + "(")
+
+            if closed_n < open_n:
+                backtrack(open_n, closed_n + 1, path + ")")
+                
+            backtrack(0, 0, "")
+
+        return res
